379aa9fa1a63487a939650533f4533935e8defb5
moe-elsadig <m.da7th@gmail.com>
2021-08-08 11:08:45 +0200

    added 3d background canvas and dark&light header

--- src/App.js
+++ src/App.js
@@ -10,8 +10,12 @@ import Portfolio from "./Components/Portfolio";
 
 import "./App.css";
 
+import * as THREE from "three";
+import bgImage from "./1198935.jpg";
+
 const App = () => {
   const [resumeData, setResumeData] = useState({});
+  const [theme, setTheme] = useState(false);
 
   useEffect(() => {
     fetch("/resumeData.json")
@@ -21,9 +25,77 @@ const App = () => {
       });
   }, []);
 
+  try {
+    const scene = new THREE.Scene();
+
+    // (view in degrees, aspect ratio, view frustrum, view frustrum)
+    const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+
+    const renderer = new THREE.WebGLRenderer({
+      canvas: document.querySelector("#bg"),
+    });
+
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.position.setZ(30);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff);
+    scene.add(ambientLight);
+
+    const spaceTexture = new THREE.TextureLoader().load(bgImage);
+
+    scene.background = spaceTexture;
+
+    function addStar() {
+      const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+      const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+      const star = new THREE.Mesh(geometry, material);
+
+      const [x, y, z] = Array(3)
+        .fill()
+        .map(() => THREE.MathUtils.randFloatSpread(100));
+
+      star.position.set(x, y, z);
+      scene.add(star);
+    }
+
+    Array(50).fill().forEach(addStar);
+
+    function moveCamera() {
+      camera.position.z += -0.01;
+      camera.position.x += -0.0002;
+      camera.rotation.y += -0.0002;
+    }
+
+    function animate() {
+      requestAnimationFrame(animate);
+      moveCamera();
+
+      // controls.update();
+
+      scene.background = theme ? new THREE.Color(0xffffff) : spaceTexture;
+      renderer.render(scene, camera);
+    }
+
+    animate();
+
+    // const canvas = document.querySelector("#bg");
+    // const banner = document.querySelector("#banner");
+
+    // let dataUrl = canvas.toDataURL();
+    // banner.style.background = "url(" + dataUrl + ")";
+  } catch (error) {
+    console.log(error);
+  }
+
   return (
-    <div className="App">
-      <Header data={resumeData.main} />
+    <div className={`App ${theme ? "" : "dark"}`}>
+      <canvas id="bg" className="inline absolute z-0 top-0 left-0"></canvas>
+      <Header
+        data={resumeData.main}
+        toggleTheme={() => setTheme(!theme)}
+        theme={theme ? "Go dark" : "Go light"}
+      />
       <About data={resumeData.main} />
       <Resume data={resumeData.resume} />
       <Portfolio data={resumeData.portfolio} />
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import TypeWriter from "react-typewriter";
 
-const Header = ({ data }) => {
+const Header = ({ data, toggleTheme, theme }) => {
   if (data) {
     var name = data.name;
     var occupation = data.occupation;
@@ -11,7 +11,10 @@ const Header = ({ data }) => {
       return (
         <li key={network.name}>
           <a href={network.url}>
-            <i className={network.className}></i>
+            <i
+              className={`
+                ${network.className} text-red-400 dark:text-white-600`}
+            ></i>
           </a>
         </li>
       );
@@ -20,63 +23,68 @@ const Header = ({ data }) => {
 
   return (
     <header id="home">
-      <nav id="nav-wrap">
+      <nav id="nav-wrap" className="bg-white dark:bg-gray-900 dark:opacity-80">
         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
           Show navigation
         </a>
         <a className="mobile-btn" href="#home" title="Hide navigation">
           Hide navigation
         </a>
-
+        <button className="absolute left-10 top-5" onClick={toggleTheme}>
+          {theme}
+        </button>
         <ul id="nav" className="nav">
-          <li className="current">
-            <a className="smoothscroll" href="#home">
+          <li>
+            <a className={`${linkColors}`} href="#home">
               Home
             </a>
           </li>
           <li>
-            <a className="smoothscroll" href="#about">
+            <a className={`${linkColors}`} href="#about">
               About
             </a>
           </li>
           <li>
-            <a className="smoothscroll" href="#resume">
+            <a className={`${linkColors}`} href="#resume">
               Resume
             </a>
           </li>
           <li>
-            <a className="smoothscroll" href="#portfolio">
+            <a className={`${linkColors}`} href="#portfolio">
               Works
             </a>
           </li>
           <li>
-            <a className="smoothscroll" href="#testimonials">
+            <a className={`${linkColors}`} href="#testimonials">
               Testimonials
             </a>
           </li>
           <li>
-            <a className="smoothscroll" href="#contact">
+            <a className={`${linkColors}`} href="#contact">
               Contact
             </a>
           </li>
         </ul>
       </nav>
 
-      <div className="row banner">
-        <div className="banner-text">
-          <h1 className="responsive-headline">
+      <div id="banner" className="row banner ">
+        <div className="banner-text z-10 ">
+          <h1 className="responsive-headline text-red-500 dark:text-white">
             <TypeWriter typing={0.5}>{name ? `I'm ${name}.` : null}</TypeWriter>
           </h1>
-          <h3>
-            Based in {city}. <span>{occupation}</span>. {description}.
+          <h3 className=" text-red-400 dark:text-green-600 backdrop-filter backdrop-blur-3xl rounded-full">
+            Based in {city}.{" "}
+            <span className="text-green-500 dark:text-white">{occupation}</span>{" "}
+            {description}.
           </h3>
           <hr />
-          <ul className="social">{networks}</ul>
+          <ul className="social color-red-400 dark:text-gray-600">
+            {networks}
+          </ul>
         </div>
       </div>
-
       <p className="scrolldown">
-        <a className="smoothscroll" href="#about">
+        <a className={`${linkColors}`} href="#about">
           <i className="icon-down-circle"></i>
         </a>
       </p>
@@ -85,3 +93,5 @@ const Header = ({ data }) => {
 };
 
 export default Header;
+
+const linkColors = `smoothscroll text-red-800 active:text-orange-900 dark:text-gray-200 active:`;
